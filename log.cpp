1f55099bd4446f9b52136f73d67018fa19a06129
Karim Benhmida <karim@MacBook-Pro-de-Karim.local>
2013-12-10 00:37:06 +0100

    Debut algo analyseLigne

--- log.cpp
+++ log.cpp
@@ -23,94 +23,6 @@
 //----------------------------------------------------------------- PUBLIC
 
 //----------------------------------------------------- Méthodes publiques
-bool log::analyseLigne (string ligne, string *cible, string *referer, int *heure)
-// Algorithme :
-//
-{
-    //bool analyseLigne(){
-    static string urlLocale = "http://intranet-if.insa-lyon.fr";
-    static size_t sizeUrlLocale = urlLocale.length();
-    
-    //cout<< UrlLocale << endl;
-    //cout << SizeUrlLocale << endl;
-    
-    int nombreOccurenceGuillemets = 0;
-    
-    for(size_t i=0; i< ligne.length() ; i++){
-        if( ligne.at(i) ==':'){
-            if( (i+2)<sizeUrlLocale){
-                char* end;
-                *heure = (int)strtol(ligne.substr(i+1,2).c_str(),&end,10);
-                
-                //cout<<"Lheure " << *heure<<endl;
-            }
-        }
-        if(ligne.at(i) == '"' && nombreOccurenceGuillemets==0 ){
-            int debut =(int)i ; //initilisation pour eviter erreurs
-            int fin = (int)ligne.length(); //initilisation pour eviter erreurs
-            
-            
-            
-            //On cherche le / symbolisant le debut de ladresse
-            for(size_t j=i; j<ligne.length();j++){
-                if(ligne.at(j) == '/'){
-                    debut = (int)j;
-                    
-                    //on cherche lespace symbolisant la fin de ladresse
-                    for(size_t k=j; k<ligne.length();k++){
-                        if(ligne.at(k) == ' '){
-                            fin = (int)k;
-                            
-                            
-                            //On cherche les guillemets fermants
-                            i=chercherGuillemetsFermants(ligne,(int)k)+1;
-                            if(i!=-1){
-                                nombreOccurenceGuillemets++;
-                            }
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            
-            *cible = ligne.substr(debut,(fin-debut));
-            //cout<<"la cible est :  "<< *cible <<endl;
-        }
-        
-        
-        if(ligne.at(i) == '"' && nombreOccurenceGuillemets==1 ){
-            int debutRef=(int)i+1;
-            int finRef = chercherGuillemetsFermants(ligne,debutRef);
-            nombreOccurenceGuillemets++;
-            
-            string refererProbable = ligne.substr(debutRef,(finRef-debutRef));
-            
-            //cout<<"Le Referer Probable est :"<<refererProbable<<endl;
-            if(refererProbable.compare(0,sizeUrlLocale,urlLocale) != 0){
-                //Pas les memes
-                *referer = refererProbable;
-            }else{
-                *referer = refererProbable.substr(sizeUrlLocale,refererProbable.length());
-            }
-            //cout<<"le referer est :  "<< *referer <<endl;
-        }
-    }
-    return true;
-} // ----- Fin de analyseLigne
-
-int log::chercherGuillemetsFermants(string l, int posDebut)
-// Algorithme :
-//
-{
-    for( size_t i=posDebut; i<l.length();i++){
-        if(l.at(i)== '"'){
-            return (int)i;
-        }
-    }
-    return -1;
-} // ----- Fin de chercherGuillemetsFermants
-
 void log::lire(string s)
 // Algorithme :
 /*
@@ -200,77 +112,6 @@ Sinon
     }
     
 } //----- Fin de lire
-
-void log::remplir(string cible, string referer, int heure)
-// Algorithme :
-/*
-entier refIndex = cibIndex = 0
-Tant que(il reste des elements dans l'index)
-    Si(referer existe deja dans l'index)
-        refIndex = i // i variable d'incrementation de la boucle
-    fin Si
-    Si(cible existe deja dans l'index)
-        cibIndex = i
-    fin Si
-    Si(referer et cible ont ete trouves)
-        break
-    fin Si
-fin Tant que
-
-Si(referer n'existe pas dans l'index)
-    insererDansIndex(referer)
-    refIndex=index.last
-fin Si
- 
-Si(cible n'existe pas dans l'index)
-    insererDansIndex(cible)
-    refCible=index.last
-fin Si
- 
-InsererDansMap(cle : cibIndex, InsererDansMap(cle : refIndex, tableauHeures[heure]++))
- 
-*/
-{
-    
-    size_t refIndex = 0;
-    size_t cibIndex = 0;
-    
-    // On vérifie si le referer et la cible sont déjà referencés, si oui on récupere l'index
-    
-    bool refExists = false;
-    bool cibExists = false;
-    
-    for(size_t i = 0; i < referencesTab.size(); i++) {
-        // cout << i << endl;
-        if(referer == referencesTab[i]) {
-            refExists = true;
-            refIndex = i;
-        }
-        if(cible == referencesTab[i]) {
-            cibExists = true;
-            cibIndex = i;
-        }
-        if(refExists==true && cibExists==true){
-            break;
-        }
-    }
-    
-    // Sinon on reference referer et cible et on recupere l'index
-    
-    if(refExists == false) {
-        referencesTab.push_back(referer);
-        //cout << referencesTab.size() ;
-        refIndex = referencesTab.size()-1;
-    }
-    if(cibExists == false) {
-        referencesTab.push_back(cible);
-        cibIndex = referencesTab.size()-1;
-    }
-    
-    
-    structure[cibIndex][refIndex].tab[heure]++;
-    
-} //----- Fin de remplir
 
 void log::testStructure()
 // Algorithme :
@@ -468,4 +309,174 @@ log::~log ( )
 //------------------------------------------------------------------ PRIVE
 
 //----------------------------------------------------- Méthodes protégées
+bool log::analyseLigne (string ligne, string *cible, string *referer, int *heure)
+// Algorithme :
+/*
 
+chaine urlLocale "http://intranet-if.insa-lyon.fr"
+
+entier = nombreOccurenceGuillemets = 0
+ 
+tant que (il reste des caracteres dans la ligne a analyser)
+    si(caractere(i) = ":")
+        si((i+2) < taillede urlLocale)
+            *heure = (int)StringToLong(TronquerLigne(i+1,i+2)
+ 
+ 
+ 
+*/
+{
+    //bool analyseLigne(){
+    static string urlLocale = "http://intranet-if.insa-lyon.fr";
+    static size_t sizeUrlLocale = urlLocale.length();
+    
+    //cout<< UrlLocale << endl;
+    //cout << SizeUrlLocale << endl;
+    
+    int nombreOccurenceGuillemets = 0;
+    
+    for(size_t i=0; i< ligne.length() ; i++){
+        if( ligne.at(i) ==':'){
+            if( (i+2)<sizeUrlLocale){
+                char* end;
+                *heure = (int)strtol(ligne.substr(i+1,2).c_str(),&end,10);
+                
+                //cout<<"Lheure " << *heure<<endl;
+            }
+        }
+        if(ligne.at(i) == '"' && nombreOccurenceGuillemets==0 ){
+            int debut =(int)i ; //initilisation pour eviter erreurs
+            int fin = (int)ligne.length(); //initilisation pour eviter erreurs
+            
+            
+            
+            //On cherche le / symbolisant le debut de ladresse
+            for(size_t j=i; j<ligne.length();j++){
+                if(ligne.at(j) == '/'){
+                    debut = (int)j;
+                    
+                    //on cherche lespace symbolisant la fin de ladresse
+                    for(size_t k=j; k<ligne.length();k++){
+                        if(ligne.at(k) == ' '){
+                            fin = (int)k;
+                            
+                            
+                            //On cherche les guillemets fermants
+                            i=chercherGuillemetsFermants(ligne,(int)k)+1;
+                            if(i!=-1){
+                                nombreOccurenceGuillemets++;
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            
+            *cible = ligne.substr(debut,(fin-debut));
+            //cout<<"la cible est :  "<< *cible <<endl;
+        }
+        
+        
+        if(ligne.at(i) == '"' && nombreOccurenceGuillemets==1 ){
+            int debutRef=(int)i+1;
+            int finRef = chercherGuillemetsFermants(ligne,debutRef);
+            nombreOccurenceGuillemets++;
+            
+            string refererProbable = ligne.substr(debutRef,(finRef-debutRef));
+            
+            //cout<<"Le Referer Probable est :"<<refererProbable<<endl;
+            if(refererProbable.compare(0,sizeUrlLocale,urlLocale) != 0){
+                //Pas les memes
+                *referer = refererProbable;
+            }else{
+                *referer = refererProbable.substr(sizeUrlLocale,refererProbable.length());
+            }
+            //cout<<"le referer est :  "<< *referer <<endl;
+        }
+    }
+    return true;
+} // ----- Fin de analyseLigne
+
+int log::chercherGuillemetsFermants(string l, int posDebut)
+// Algorithme :
+//
+{
+    for( size_t i=posDebut; i<l.length();i++){
+        if(l.at(i)== '"'){
+            return (int)i;
+        }
+    }
+    return -1;
+} // ----- Fin de chercherGuillemetsFermants
+
+void log::remplir(string cible, string referer, int heure)
+// Algorithme :
+/*
+ entier refIndex = cibIndex = 0
+ Tant que(il reste des elements dans l'index)
+ Si(referer existe deja dans l'index)
+ refIndex = i // i variable d'incrementation de la boucle
+ fin Si
+ Si(cible existe deja dans l'index)
+ cibIndex = i
+ fin Si
+ Si(referer et cible ont ete trouves)
+ break
+ fin Si
+ fin Tant que
+ 
+ Si(referer n'existe pas dans l'index)
+ insererDansIndex(referer)
+ refIndex=index.last
+ fin Si
+ 
+ Si(cible n'existe pas dans l'index)
+ insererDansIndex(cible)
+ refCible=index.last
+ fin Si
+ 
+ InsererDansMap(cle : cibIndex, InsererDansMap(cle : refIndex, tableauHeures[heure]++))
+ 
+ */
+{
+    
+    size_t refIndex = 0;
+    size_t cibIndex = 0;
+    
+    // On vérifie si le referer et la cible sont déjà referencés, si oui on récupere l'index
+    
+    bool refExists = false;
+    bool cibExists = false;
+    
+    for(size_t i = 0; i < referencesTab.size(); i++) {
+        // cout << i << endl;
+        if(referer == referencesTab[i]) {
+            refExists = true;
+            refIndex = i;
+        }
+        if(cible == referencesTab[i]) {
+            cibExists = true;
+            cibIndex = i;
+        }
+        if(refExists==true && cibExists==true){
+            break;
+        }
+    }
+    
+    // Sinon on reference referer et cible et on recupere l'index
+    
+    if(refExists == false) {
+        referencesTab.push_back(referer);
+        //cout << referencesTab.size() ;
+        refIndex = referencesTab.size()-1;
+    }
+    if(cibExists == false) {
+        referencesTab.push_back(cible);
+        cibIndex = referencesTab.size()-1;
+    }
+    
+    
+    structure[cibIndex][refIndex].tab[heure]++;
+    
+} //----- Fin de remplir
