8e3d54b9b1cab002a338763eaceb72df4a84a5c7
Mehdi Kitane <mehdi_kitane@msn.com>
2013-12-12 23:54:18 +0100

    PseudoCode Commande

--- Commande.cpp
+++ Commande.cpp
@@ -16,7 +16,17 @@
 
 
 namespace commande {
-    bool checkIfValidNameFile(string s){
+    bool checkIfValidNameFile(string s)
+    // Algorithme :
+    // Si taille du string inférieur à 4
+    //      Retourne faux
+    // Fin si
+    // Si le string finit par .log ou par .txt
+    //      Retourne vrai
+    // Fin si
+    //
+    // Retourne faux
+    {
         if (s.size() <= 4) {
             return false;
         }
@@ -27,7 +37,17 @@ namespace commande {
         
         
     }
-    bool checkIfValidDotFile(string s){
+    bool checkIfValidDotFile(string s)
+    // Algorithme :
+    // Si taille du string inférieur à 4
+    //      Retourne faux
+    // Fin si
+    // Si le string finit par .dot
+    //      Retourne vrai
+    // Fin si
+    //
+    // Retourne faux
+    {
         if (s.size() <= 4) {
             return false;
         }
@@ -36,14 +56,33 @@ namespace commande {
         }
         return false;
     }
-    bool checkIfOption(string s){
+    bool checkIfOption(string s)
+    // Algorithme :
+    // Si le string commence par un tiret
+    //      Retourne vrai
+    // Sinon
+    //      Retourne faux
+    //  Fin si
+    {
         if(s[0]=='-'){
             return true;
         }
         return false;
     }
     
-    bool checkIfNeedParam(string arg){
+    bool checkIfNeedParam(string arg)
+    // Algorithme :
+    // Si le string est egal à 'g'
+    //      Retourne vrai
+    // Fin si
+    // Si le string est egal à 'l'
+    //      Retourne vrai
+    // Fin si
+    // Si le string est egal à 't'
+    //      Retourne vrai
+    // Fin si
+    // Retourne faux sinon
+    {
         if(arg.compare("g")==0){
             return true;
         }
@@ -55,12 +94,19 @@ namespace commande {
         }
         return false;
     }
-    bool checkIfValidParam(string param){
+    bool checkIfValidParam(string param)
+    // Algorithme :
+    // Si le param de l'option n'a pas la syntaxe d'une option et n'a pa la syntaxe du fichier Log
+    // Fin si
+    {
         //si n'a pas la syntaxe d'une option et n'a pa la syntaxe du fichier Log
         return (!checkIfOption(param) && !checkIfValidNameFile(param));
     }
-    string getNameOfOption(string s){
+    string getNameOfOption(string s)
+    // Algorithme :
+    // Retourner de la seconde à la derniere lettre de s
+    {
         return s.substr(1,s.size());
     }
-}
+} //Fin du namespace commande
 
